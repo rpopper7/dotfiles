f02432c44eee9481d96d9265b369a2a1a3625d11
Rebecca Popper <rpopper7@users.noreply.github.com>
2025-09-10 22:19:29 -0400

    spacing

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -3,7 +3,7 @@ return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
-	theme = "catppuccin",
+	    theme = "catppuccin",
     },
     config = function(_, opts)
 	require("lualine").setup({
